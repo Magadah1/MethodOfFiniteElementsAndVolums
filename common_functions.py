8208f0ce31e79c2d73f8c689ba8f0910dcd02c34
Dmitry Stolyarov <vk153547424@gmail.com>
2024-09-29 16:32:17 +0300

    Функции формы для треугольника и прямоугольника. И ещё какая-то функция.

--- common_functions.py
+++ common_functions.py
@@ -12,19 +12,69 @@ def get_triangle_area_made_by_vertices(vl : data.Vertex, vm : data.Vertex, vn : 
     )
 
 
-def triangle_form(vl : data.Vertex, vm : data.Vertex, vn : data.Vertex, v : data.Vertex, f):
+def triangle_form(v1 : data.Vertex, v2 : data.Vertex, v3 : data.Vertex, v : data.Vertex, v_id : int):
     """
-    Вычисляет значение функции треугольной формы в треугольнике (vl, vm, vn) в точке v на функции f.
+    Функция формы треугольника в Вершине v, относительно Вершины с номером v_id = 1, 2, 3.
     """
-    Fl = f(vl.x, vl.y)
-    Fm = f(vm.x, vm.y)
-    Fn = f(vn.x, vn.y)
+    if v_id > 3 or v_id < 1:
+        return -1
 
-    area = get_triangle_area_made_by_vertices(vl, vm, vn)
+    triangle_area = get_triangle_area_made_by_vertices(v1, v2, v3)
 
-    Fl_area = get_triangle_area_made_by_vertices(v, vm, vn)
-    Fm_area = get_triangle_area_made_by_vertices(vl, v, vn)
-    Fn_area = get_triangle_area_made_by_vertices(vl, vm, v)
+    if v_id == 1:
+        return get_triangle_area_made_by_vertices(v, v2, v3) / triangle_area
+    elif v_id == 2:
+        return get_triangle_area_made_by_vertices(v1, v, v3) / triangle_area
+    elif v_id == 3:
+        return get_triangle_area_made_by_vertices(v1, v2, v) / triangle_area
 
-    return (Fl * Fl_area + Fm * Fm_area + Fn * Fn_area) / area
 
+def full_triangle_form_with_function_value(v1 : data.Vertex, v2 : data.Vertex, v3 : data.Vertex, v : data.Vertex, f):
+    """
+    Вычисляет значение функции треугольной формы в Вершине v на функции f.
+    """
+    F1 = f(v1.x, v1.y)
+    F2 = f(v2.x, v2.y)
+    F3 = f(v3.x, v3.y)
+
+    F1_area = triangle_form(v1, v2, v3, v, 1)
+    F2_area = triangle_form(v1, v2, v3, v, 2)
+    F3_area = triangle_form(v1, v2, v3, v, 3)
+
+    return F1 * F1_area + F2 * F2_area + F3 * F3_area
+
+
+def rectangle_form(v1 : data.Vertex, v2 : data.Vertex, v3 : data.Vertex, v4 : data.Vertex, v : data.Vertex, v_id : int):
+    """
+    Функция формы прямоугольника в Вершине v, относительно Вершины с номером v_id = 1, 2, 3, 4.
+    """
+    if v_id > 4 or v_id < 1:
+        return -1
+
+    rectangle_area = get_triangle_area_made_by_vertices(v1, v2, v3) + get_triangle_area_made_by_vertices(v1, v3, v4)
+
+    if v_id == 1:
+        return (get_triangle_area_made_by_vertices(v, v2, v3) + get_triangle_area_made_by_vertices(v, v3, v4)) / rectangle_area
+    elif v_id == 2:
+        return (get_triangle_area_made_by_vertices(v1, v, v3) + get_triangle_area_made_by_vertices(v1, v3, v4)) / rectangle_area
+    elif v_id == 3:
+        return (get_triangle_area_made_by_vertices(v1, v2, v) + get_triangle_area_made_by_vertices(v1, v, v4)) / rectangle_area
+    elif v_id == 4:
+        return (get_triangle_area_made_by_vertices(v1, v2, v3) + get_triangle_area_made_by_vertices(v1, v3, v)) / rectangle_area
+
+
+def full_rectangle_form_with_function_value(v1 : data.Vertex, v2 : data.Vertex, v3 : data.Vertex, v4 : data.Vertex, v : data.Vertex, f):
+    """
+    Вычисляет значение функции прямоугольной формы в Вершине v на функции f.
+    """
+    F1 = f(v1.x, v1.y)
+    F2 = f(v2.x, v2.y)
+    F3 = f(v3.x, v3.y)
+    F4 = f(v4.x, v4.y)
+
+    F1_area = rectangle_form(v1, v2, v3, v4, v, 1)
+    F2_area = rectangle_form(v1, v2, v3, v4, v, 2)
+    F3_area = rectangle_form(v1, v2, v3, v4, v, 3)
+    F4_area = rectangle_form(v1, v2, v3, v4, v, 4)
+
+    return F1 * F1_area + F2 * F2_area + F3 * F3_area + F4 * F4_area
